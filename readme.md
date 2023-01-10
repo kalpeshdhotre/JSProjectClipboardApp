d54012b9dfa089377abc2725df4c58173f531118
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2023-01-10 18:35:48 +0530

    readme and Live link added

---
+++ readme.md
@@ -0,0 +1,5 @@
+> ## by: Kalpesh KD
+
+# Clipboard App
+
+## [Click here for live link](https://kdjsclipboardapp.netlify.app)
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let submitButton = document.querySelector(".btnSubmit");
 
 submitButton.addEventListener(`click`, () => {
-    if (submitButton.innerHTML == `New`) {
-        submitButton.innerText = "Submit";
-        console.log("in if loop");
-        document.querySelector("#textbox").removeAttribute("readonly");
-        return;
-    }
+   if (submitButton.innerHTML == `New`) {
+      submitButton.innerText = "Submit";
+      console.log("in if loop");
+      document.querySelector("#textbox").removeAttribute("readonly");
+      return;
+   }
 
-    let userData = document.querySelector(`#textbox`).value;
-    console.log(`Data entered is ${userData}`);
-    // console.log("Hello World");
-    navigator.clipboard.writeText(userData);
-    submitButton.innerHTML = "New";
-    document.querySelector("#textbox").setAttribute("readonly", "readonly");
+   let userData = document.querySelector(`#textbox`).value;
+   console.log(`Data entered is ${userData}`);
+   // console.log("Hello World");
+   navigator.clipboard.writeText(userData);
+   document.querySelector(`#textbox`).value = "";
+   submitButton.innerHTML = "New";
+   document.querySelector("#textbox").setAttribute("readonly", "readonly");
 });
